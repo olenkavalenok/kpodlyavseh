ad1fb4c31c9d532a54a35fd951ff6442cbe4b6fc
Katya959 <katya.safutina@bk.ru>
2020-12-14 21:48:21 +0300

    add

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace KPO_Lab
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        ///// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
